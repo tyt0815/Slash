b49a260cb0c3d18aa855ab6393dc6bfc769b1e0a
tyt0815 <yuy0815@icloud.com>
2023-07-02 19:07:56 +0900

    Soul Drift

--- Source/Slash_/Private/Items/Soul.cpp
+++ Source/Slash_/Private/Items/Soul.cpp
@@ -3,6 +3,47 @@
 
 #include "Items/Soul.h"
 #include "Interfaces/PickupInterface.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+void ASoul::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	const double LocationZ = GetActorLocation().Z;
+	if (LocationZ > DesiredZ)
+	{
+		const FVector DeltaLocation = FVector(0.f, 0.f, DriftRate * DeltaTime);
+		AddActorWorldOffset(DeltaLocation);
+	}//endif
+}
+
+void ASoul::BeginPlay()
+{
+	Super::BeginPlay();
+
+	const FVector Start = GetActorLocation();
+	const FVector End = GetActorLocation() - FVector(0.f, 0.f, 2000.f); // Line Tracing¿ª ¿ß«— ∫§≈Õ
+
+	TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypes;
+	ObjectTypes.Add(EObjectTypeQuery::ObjectTypeQuery1);
+	TArray<AActor*> ActorsToIgnore;
+	ActorsToIgnore.Add(this);
+	ActorsToIgnore.Add(GetOwner());
+	FHitResult HitResult;
+
+	UKismetSystemLibrary::LineTraceSingleForObjects(
+		this,
+		Start,
+		End,
+		ObjectTypes,
+		false,
+		ActorsToIgnore,
+		EDrawDebugTrace::ForDuration,
+		HitResult,
+		true
+	);
+	DesiredZ = HitResult.ImpactPoint.Z + 50.f;
+}
 
 void ASoul::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
